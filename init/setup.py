e6b93e1b5fc53ea90140608f8b6d89f3331c10cb
Per Nilsson <per.nilsson@formidablelabs.com>
2016-06-30 14:00:05 -0700

    Convert package name to underscored

--- init/setup.py
+++ init/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='<%= packageName %>',
+    name='<%= packageNameUnderscored %>',
     version='0.0.1',
     author='<%= packageGitHubOrg %>',
     packages=['dash', 'dash/development'],
     license='MIT',
-    description='<%= packageDescription || packageName %>',
+    description='<%= packageDescription || packageNameUnderscored %>',
     install_requires=['flask', 'plotly', 'flask-cors', 'dash.ly']
 )
