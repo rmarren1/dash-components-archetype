f80f88737b61c45c7aa40f54d51aa53f0fa9b752
Per Nilsson <per.nilsson@formidablelabs.com>
2016-07-31 11:42:56 -0700

    Dev bundle should have same build target as prod

--- config/webpack/partials/outputDev.js
+++ config/webpack/partials/outputDev.js
@@ -4,13 +4,17 @@ var path = require('path');
 var partial = require('webpack-partial').default;
 
 var ROOT = process.cwd();
-var BUILD = path.join(ROOT, 'build');
+var BUILD_PATH = path.join(ROOT, 'lib');
+
+// TEMP Change to dynamic library name
+var LIBRARY_NAME = 'dash_core_components';
 
 module.exports = function (config) {
     return partial(config, {
         output: {
-            path: BUILD,
-            publicPath: '/build/',
+            library: LIBRARY_NAME,
+            libraryTarget: 'this', // Could be 'umd'
+            path: BUILD_PATH,
             filename: '[name].js'
         }
     });
