47586519e1479b610fadff24fc6ec68abc96c291
Carson Sievert <cpsievert1@gmail.com>
2017-10-05 16:54:48 -0500

    take advantage of transpile_write()

--- init/r/inst/transpile.R
+++ init/r/inst/transpile.R
@@ -1,8 +1,7 @@
-if (!require(remotes)) install.packages("remotes")
 if (!require(dashTranspileR)) remotes::install_github("plotly/dashTranspileR")
-if (!require(roxygen2)) install.packages("roxygen2")
 
-md <- dashTranspileR::transpile(
+library(dashTranspileR)
+md <- transpile(
    "lib/metadata.json", namespace = "<%= packageNameUnderscored %>"
 )
-writeLines(md[['text']], "R/components.R")
+transpile_write(md)
