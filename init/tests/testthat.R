468744231ab2b3ead8d9c0264bc6dc6257c2228e
Carson Sievert <cpsievert1@gmail.com>
2017-10-05 18:57:53 -0500

    add a basic R testing template

---
+++ init/tests/testthat.R
@@ -0,0 +1,6 @@
+library(testthat)
+library(<%= packageNameCamelCase %>)
+
+test_check("<%= packageNameCamelCase %>")
+
+
---
+++ init/tests/testthat/test-basic.R
@@ -0,0 +1,7 @@
+context("Component test")
+
+test_that("Component can be created", {
+
+  expect_is(examplecomponent(), "dash_component")
+
+})
