1128b6f101eda30d0a7c2941a548db0d76835fb4
Carson Sievert <cpsievert1@gmail.com>
2017-10-06 12:59:42 -0500

    new versions of dashTranspileR/dasher will leverage package.json and packageNameUnderscored/

--- init/inst/transpile.R
+++ init/inst/transpile.R
@@ -1,7 +1,4 @@
 if (!require(dashTranspileR)) remotes::install_github("plotly/dashTranspileR")
 
 library(dashTranspileR)
-md <- transpile(
-   "lib/metadata.json", namespace = "<%= packageNameUnderscored %>"
-)
-transpile_write(md)
+transpile_write(transpile())
